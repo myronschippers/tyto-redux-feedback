c8ab3f24df72e26ec6b12f35755f627f22064444
Myron <dubtwee@gmail.com>
2019-12-16 09:39:08 -0600

    setting up redux for the react app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// What data structure should look like
+// {
+//     feeling: 0,
+//     understanding: 0,
+//     support: 0,
+//     comments: '',
+// }
+function feedbackReducer(state = {}, action) {
+    return state;
+}
+
+const storeInstance = createStore(
+    // pass our reducers
+    combineReducers({
+        feedbackReducer,
+    }),
+    // pass our middleware
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
