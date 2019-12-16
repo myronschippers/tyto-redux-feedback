97ff667d98cfe766cc2a2455d2e984629c8eb005
Myron <dubtwee@gmail.com>
2019-12-16 11:07:11 -0600

    finalizing the Review page and submission

--- src/components/pages/Review/Review.js
+++ src/components/pages/Review/Review.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class Review extends Component {
+    clickSubmit = (event) => {
+        axios({
+            method: 'POST',
+            url: '/api/feedback',
+            data: this.props.store.feedbackReducer
+        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                alert('Your feedback could not be submitted at this time.');
+                console.warn(err);
+            });
+    }
+
     render() {
         return (
             <div>
-                Review
+                <h2>Review Your Feedback</h2>
+                <p>Feelings: {this.props.store.feedbackReducer.feeling}</p>
+                <p>Understanding: {this.props.store.feedbackReducer.understanding}</p>
+                <p>Support: {this.props.store.feedbackReducer.support}</p>
+                <p>Comments: {this.props.store.feedbackReducer.comments}</p>
+                <div>
+                    <button onClick={this.clickSubmit}>SUBMIT</button>
+                </div>
             </div>
         );
     }
 }
 
-export default connect()(Review);
+const mapStoreToProps = (store) => {
+    return {
+        store,
+    } 
+};
+
+export default connect(mapStoreToProps)(Review);
