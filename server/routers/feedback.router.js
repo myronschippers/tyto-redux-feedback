51a0c52aa4837a97cf312d914beb205064a5eb47
Myron <dubtwee@gmail.com>
2019-12-16 10:00:31 -0600

    creating POST API endpoint for feedback

--- server/routers/feedback.router.js
+++ server/routers/feedback.router.js
@@ -4,7 +4,32 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.sendStatus(201);
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                        VALUES ($1, $2, $3, $4);`;
+    const feedback = req.body;
+    // feedback = {
+    //     feeling: 0,
+    //     support: 0,
+    //     understanding: 0,
+    //     comments: ''
+    // }
+
+    pool.query(
+        queryText,
+        [
+            feedback.feeling,
+            feedback.understanding,
+            feedback.support,
+            feedback.comments
+        ]
+    )
+        .then((response) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 })
 
 module.exports = router;
